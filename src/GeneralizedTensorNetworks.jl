d054f04dce4fc4906df54d4f3b839ce77ed91ae3
Jason Morton <jason.morton@gmail.com>
2015-03-29 09:49:47 -0700

    includes for new representations

--- src/GeneralizedTensorNetworks.jl
+++ src/GeneralizedTensorNetworks.jl
@@ -1,3 +1,16 @@
 module GeneralizedTensorNetworks
+# Core typeclass definitions
 include("MonoidalCategories.jl")
+
+#Symbolic and expression categories
+include("FiniteTensorSchemes.jl")
+
+# Graphical Categories
+include("WiresBoxes.jl")
+
+# Quantitative Categories
+include("BinBraKet.jl")
+
+# Operadic categories
+
 end
