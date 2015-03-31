0e31428f4ca8004304c68307573482fb871871d7
Jason Morton <jason@jasonmorton.com>
2015-03-31 08:51:48 -0400

    test cleanup

--- test/testFiniteTensorSignatures.jl
+++
@@ -1,9 +0,0 @@
-using FiniteTensorSignatures
-using Base.test
-
-@test FTS("f:a⊗b→c,g:a→b⊗c")==FTS("f:a⊗b→c, g:a→b⊗c")
-
-T = FTS("f:a⊗b→c,g:a→b⊗c")
-
-O=FiniteTensorSignatures.OWord
-@test O(:A)⊗O(:())⊗munit(O((:B))).word==O(:A).word
